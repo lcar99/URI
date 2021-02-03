cb99381812fc5724f980979e82c29e2d030b69c3
lcar99 <65090656+lcar99@users.noreply.github.com>
2021-02-03 04:03:07 -0300

    Create 2510.cpp

---
+++ C++/2510.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    char vilao[30];
+    scanf ("%d", &t);
+    while (t--){
+        scanf ("%s", vilao);
+        printf ("Y\n");
+    }
+    return 0;
+}
